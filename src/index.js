6843ee8ba253fc41ae138b8ee4e678cf6467af3b
Allefy Nepomucena <allefy.s.nepomucena@gmail.com>
2021-10-15 20:15:06 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+const { request, response } = require('express')
 const express = require('express')
 
 const app = express()
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+/**MÉTODOS HTTP
+ *
+ * GET: Buscar informações no back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PACHT: Alternar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+app.get('/projects', (request, response) => {
+  return response.json(['Projeto 1', 'Projeto 2'])
+})
+app.post('/projects', (request, response) => {
+  return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
 })
 
+app.put('/projects/:id', (request, response) => {
+  return response.json(['Projeto 4', 'Projeto 2', 'Projeto 3'])
+})
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json(['Projeto 2', 'Projeto 3'])
+})
 app.listen(3333, () => {
   console.log('Funcionou Caralhooo🚀')
 })
