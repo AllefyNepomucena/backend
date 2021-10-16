43cbe976b6079fbaf1446fb35f4c3657501e3112
Allefy Nepomucena <allefy.s.nepomucena@gmail.com>
2021-10-16 17:08:53 -0300

    Criando Tipos de Parâmetros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { request, response } = require('express')
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 /**MÉTODOS HTTP
  *
  * GET: Buscar informações no back-end
@@ -9,14 +10,30 @@ const app = express()
  * PUT/PACHT: Alternar uma informação no back-end
  * DELETE: Deletar uma informação no back-end
  */
+/**
+ * TIPOS DE PARÂMETROS
+ * Query Params: Filtros e Paginação
+ * Route Params: Identifiacar recursos (Atualizar/Deletar)
+ * Request Params: Conteúdo na hora de criar ou editar um recurso(JSON)
+ */
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query
+
+  console.log(title)
+  console.log(owner)
+
   return response.json(['Projeto 1', 'Projeto 2'])
 })
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body
+  console.log(title)
+  console.log(owner)
   return response.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
 })
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params
+  console.log(id)
   return response.json(['Projeto 4', 'Projeto 2', 'Projeto 3'])
 })
 
